9e6d324fd00689b8091ea7fb94cae824e91a701a
n451 <2020200706@ruc.edu.cn>
2023-11-19 00:02:33 +0800

    list catalog, install scripts and update README

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ WIP
 - [x] access filesystem and file edit
 - [x] split window remote repl
 - [x] live reload on save
+- [ ] auto make folder and option to unmount on vimexit
 - [ ] install scripts wit github links (easier through madien remote repl)
 - [ ] read, select and install from the catalog (through maiden cli)
 - [ ] find a way for lua_ls to ignore norns global functions
--- after/plugin/maiden.lua
+++ after/plugin/maiden.lua
@@ -37,9 +37,9 @@ end
 
 vim.api.nvim_create_user_command("MaidenStart", function()
 	local addr = vim.fn.input("Enter norns address: ")
-  if addr == "" then
-    addr = defaults.addr
-  end
+	if addr == "" then
+		addr = defaults.addr
+	end
 	sync(defaults.dir, addr)
 end, {})
 
@@ -48,10 +48,10 @@ vim.api.nvim_create_user_command("MaidenEnd", function()
 end, {})
 
 local function load_script()
-  local host = defaults.addr
+	local host = defaults.addr
 	Job:new({
 		command = "maiden-remote-repl",
-    args = { '--host', host, 'send', "norns.script.load(norns.state.script)" },
+		args = { "--host", host, "send", "norns.script.load(norns.state.script)" },
 		on_exit = function(j, return_val)
 			print("Return value:", return_val)
 			print("Output:", table.concat(j:result(), "\n"))
@@ -62,12 +62,10 @@ local function load_script()
 	}):start() -- Use sync to wait for the job to finish
 end
 
-vim.api.nvim_create_autocmd(
-  {"BufWritePost"},
-  {
-    pattern = defaults.dir.."/*.lua",
-    callback = load_script,}
-)
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	pattern = defaults.dir .. "/*.lua",
+	callback = load_script,
+})
 
 -- TODO: OR A FUNCTION TO MANUALY UPDATE
 -- vim.api.nvim_create_user_command("MaidenUpdate",
@@ -79,4 +77,70 @@ vim.api.nvim_create_autocmd(
 --
 -- )
 
--- TODO: FUNCTION TO UPDATE FORM A GIT REPO
+-- TODO: list the catalog
+
+local catalog = {}
+
+local function clean_string(input_string)
+	local cleaned_elements = {}
+	for elem1, elem2, elem3 in string.gmatch(input_string, "(.-)\t\t+(.-)\t\t+(.+)") do
+		cleaned_elements[1] = elem1:match("^%s*(.-)%s*$")
+		cleaned_elements[2] = elem2:match("^%s*(.-)%s*$")
+		cleaned_elements[3] = elem3:match("^%s*(.-)%s*$")
+	end
+	return cleaned_elements
+end
+
+local function clean_catalog()
+	local res = {}
+	for i, v in ipairs(catalog) do
+		table.insert(res, clean_string(v))
+	end
+	return res
+end
+
+local function list_catalog()
+	Job:new({
+		command = "ssh",
+		args = {
+			"we@192.168.43.179",
+			"maiden/maiden catalog list",
+		},
+		on_stdout = function(j, data)
+			table.insert(catalog, data)
+		end,
+		on_exit = function(j, exit_code)
+			if exit_code == 0 then
+				print("Command executed successfully")
+			else
+				print("Error executing the command. Error code:", exit_code)
+			end
+		end,
+	}):sync()
+	return clean_catalog()
+end
+
+-- TODO: FUNCTION TO install a script
+local function install(script)
+	Job:new({
+		command = "ssh",
+		args = {
+			"we@192.168.43.179",
+			"maiden/maiden project install " .. script
+		},
+		on_stdout = function(_, data)
+			table.insert(catalog, data)
+		end,
+		on_exit = function(_, exit_code)
+			if exit_code ~= 0 then
+				print("Error executing the command. Error code:", exit_code)
+			end
+		end,
+	}):start()
+end
+
+vim.api.nvim_create_user_command("MaidenInstall", function()
+	local package = vim.fn.input("Enter package to install: ")
+	install(package)
+end, {})
+
